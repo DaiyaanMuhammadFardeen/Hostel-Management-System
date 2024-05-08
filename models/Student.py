6b0260345931680761baf0d12d70a080f15ae7c8
Daiyaan Muhammad <daiyaan.villain@gmail.com>
2024-05-08 19:51:13 +0600

    Removed the long ass constructor

--- models/Student.py
+++ models/Student.py
@@ -1,4 +1,7 @@
+import uuid
+
 import User
+
 
 class Student(User):
     id = int()
@@ -7,12 +10,4 @@ class Student(User):
     father = str()
     mother = str()
     email = str()
-    user_id = int()
-    def __init__(self,  id, name, contact, father, mother, email):
-        self.id = id
-        self.name = name
-        self.contact = contact
-        self.father = father
-        self.mother = mother
-        self.email = email
-        self.user_id = User.get_user_id()
+    user_id = uuid.uuid4()
