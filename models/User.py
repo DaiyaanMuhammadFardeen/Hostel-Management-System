80f4e0ba93a565c3c5cd98fad4377f2ec563bf0d
Daiyaan Muhammad <daiyaan.villain@gmail.com>
2024-05-08 19:38:13 +0600

    Swtiched to UUID instead of random int

--- models/User.py
+++ models/User.py
@@ -1,5 +1,8 @@
+import uuid
+
+
 class User:
-    id = int()
+    id = uuid.uuid4()
     username = str()
     password = str()
     role = str()
@@ -7,5 +10,7 @@ class User:
 
 def get_user_id(self):
     return self.id
+
+
 def get_user_username(self):
     return self.username
