542816e324a6833a21fab27e76ab69733849014e
Daiyaan Muhammad <daiyaan.villain@gmail.com>
2024-05-08 19:52:27 +0600

    "Add student functionality in the UI"

--- ui/UI.py
+++ ui/UI.py
@@ -1,13 +1,17 @@
 import os
 
+from models.Student import Student
+from services.StudentService import StudentService
+
+studentService = StudentService()
 class UI:
-    def clear(self):
+    def clearScreen(self):
         os.system("cls")
 
     username = str()
     password = str()
     def showLogin(self):
-        self.clear()
+        self.clearScreen()
         print("Welcome to the Hostel Management System")
         print("Please login to continue")
         print("Username: ", end="")
@@ -24,7 +28,7 @@ class UI:
             self.studentMode()
 
     def adminMode(self):
-        self.clear()
+        self.clearScreen()
         print(
             """Welcome {{username}} to Hostel Management Portal.
             Please select one of the following options:
@@ -47,3 +51,13 @@ class UI:
                 self.searchStudent()
             case _:
                 self.adminMode()
+
+    def addStudent(self):
+        self.clearScreen()
+        new_student = Student()
+        new_student.name = input("Name: ")
+        new_student.contact = input("Phone no: ")
+        new_student.father = input("Father Name: ")
+        new_student.mother = input("Mother Name: ")
+        new_student.email = input("Email: ")
+        studentService.addStudent(new_student)
